ec4949b862beb02508a6adb7a4283e6be41aff1e
zunaira-jadoon13 <241156@students.au.edu.pk>
2025-01-07 11:35:42 +0500

    Update project 9.cpp

--- project 9/project 9.cpp
+++ project 9/project 9.cpp
@@ -28,14 +28,17 @@ int main() {
             break;
         case 4:
             removeMenuItem(menu);
-            break;
+            break;\
         case 5:
+            orderItems(menu);
+            break;
+        case 6:
             cout << "Exiting...\n";
             break;
         default:
             cout << "Invalid choice. Please try again.\n";
         }
-    } while (choice != 5);
+    } while (choice != 6);
 
     return 0;
 }
